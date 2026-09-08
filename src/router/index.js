3a73ea07064e928797be20d75ec761a630eec4b0
xxxyuju <youju2316@gmail.com>
2026-09-08 16:08:07 +0900

    feat: 회원가입 화면 및 API 연동

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,25 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
     },
+    {
+      path: '/signup',
+      redirect: '/signup/info',
+    },
+    {
+      path: '/signup/info',
+      name: 'signup-info',
+      component: () => import('@/views/signup/SignupInfoView.vue'),
+    },
+    {
+      path: '/signup/terms',
+      name: 'signup-terms',
+      component: () => import('@/views/signup/SignupTermsView.vue'),
+    },
+    {
+      path: '/signup/complete',
+      name: 'signup-complete',
+      component: () => import('@/views/signup/SignupCompleteView.vue'),
+    },
   ],
 })
 
